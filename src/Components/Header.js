51376d75707b612bc6b8bd23d6a670921239775c
Joshua <wisconsintorres@gmail.com>
2021-01-25 16:46:40 -0600

    add images and header edits

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,52 @@
 import React from 'react';
+import './Header.css';
 import SearchIcon from '@material-ui/icons/Search'
+import HomeIcon from '@material-ui/icons/Home'
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+import logo from '../Images/BB.png';
+
+
 
 const Header = () => {
     return (
         <div className='header'>
             <div className="header_left">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/0/0d/AIMMP%40Facebook.png" alt="FB logo"/>
+
+                <img src={logo} alt="BB logo"/>
+
+                <div className="header_input">
+                    <SearchIcon fontSize="small"/>
+                    <input placeholder="Search site" type="text"/>
+                </div>
+
+            </div>
+            <div className="header_center">
+
+
+                <div className="header_option header_option--active">
+                    <IconButton>
+                        <HomeIcon fontSize="large" />
+                    </IconButton>
+                </div>
+
+
+                <div className="header_option">
+                    <IconButton>
+                        <Avatar fontSize="large" />
+                    </IconButton>
+                </div>
+
             </div>
 
-            <div className="header_input">
-                <input placeholder="Search site" type="text"/>
+
+
+            <div className="header_right">
+
+
             </div>
-            <div className="header_option">
-                <SearchIcon />
-            </div>
+
+
             
         </div>
     );
